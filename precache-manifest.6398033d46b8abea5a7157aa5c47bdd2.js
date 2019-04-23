91731440b1661cb1c9508957f9742ae23857fc6f
Emmanuel Orozco <emmanuel.orozco@kapten.com>
2019-04-23 12:05:14 +0200

    Updates

---
+++ precache-manifest.6398033d46b8abea5a7157aa5c47bdd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76233aca6632f33b609add9647d91d75",
+    "url": "/embedded-vs-reference-mongo-db/index.html"
+  },
+  {
+    "revision": "e43a49e3af0144c7b7b0",
+    "url": "/embedded-vs-reference-mongo-db/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/embedded-vs-reference-mongo-db/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "e43a49e3af0144c7b7b0",
+    "url": "/embedded-vs-reference-mongo-db/static/js/main.3d2d82d2.chunk.js"
+  },
+  {
+    "revision": "6177fbb7f59dfac61d57",
+    "url": "/embedded-vs-reference-mongo-db/static/js/runtime~main.96f4dff3.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+
+importScripts(
+  "/embedded-vs-reference-mongo-db/precache-manifest.6398033d46b8abea5a7157aa5c47bdd2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/embedded-vs-reference-mongo-db/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
